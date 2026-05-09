8cf37accd90cdb4662f187df476c9480b5afe824
AsadRafi74 <asadrafi774@gmail.com>
2026-05-09 19:09:08 +0500

    dictionary in python
    dictionary and its method which is commonly used in python

---
+++ dic.py
@@ -0,0 +1,23 @@
+info = {
+    "name": "dic",
+    "subject":{
+        "math": 80,
+        "english": 90,
+        "physics": 85
+    },  #nested dictionary
+    "topic" : ("dic","ok","unorder"),
+    "age":24,
+    "learing":"python",
+    12.04:42.21
+    }
+
+# info["surname"] = "Rafi"
+# print(info)               #adding new key value pair in dictionary
+# print(info.keys())        #print all keys in dictionary
+# print(len(info))          #print length of dictionary
+# print(list(info.keys()))  #print all keys in dictionary in list form
+#print(info.items())        #print all key value pair in dictionary in tuple form
+# pairs = list(info.items())
+# print(pairs[4])            #print 4th key value pair in dictionary
+#print(info.get("subject"))   #print value of key "subject" in dictionary 
+print(info)
