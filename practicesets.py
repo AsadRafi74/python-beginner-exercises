3246a65ec3e24747832ecb205780b43793bf8f7a
AsadRafi74 <asadrafi774@gmail.com>
2026-05-10 11:25:44 +0500

    dictionary & sets practice
    practice for beginners as  python developer

---
+++ practicesets.py
@@ -0,0 +1,36 @@
+# dic = {
+#     "table": ["a piece of furniture", "a list of facts & figures"],
+#     "cat": " a small animal"
+# }
+# print(dic)
+  
+ #find the numbers of classes for following subjects 
+  
+# subjects ={"python","java","c++","python","javascript","java",
+#                 "python","java","c++","c"}
+# print(len(subjects))
+
+
+#take input from user and add it to set
+
+# marks = {}
+# x = int(input("enter phy :"))
+# marks.update({"phy":x})
+
+
+# x = int(input("enter chem :"))
+# marks.update({"chem":x})
+
+# x = int(input("enter math :"))
+# marks.update({"math":x})
+
+# x = int(input("enter eng :"))
+# marks.update({"eng":x})
+
+# print(marks)
+
+
+#storing two values which looks same in python using string in sets
+
+# set = {9,"9.0"}
+# print(set)
---
+++ sets.py
@@ -0,0 +1,20 @@
+# set = set()
+# set.add(3)         #add 3 to set
+# set.add("a")
+# set.add("c")
+# set.add("b")
+# set.add(4.5)
+# set.add(6)
+# set.add(1)         #add 1 to set
+# set.remove(1)      #remove 1 from set
+# set.pop()          #remove and return an arbitrary element from set
+set1 ={1,2,3,4,5,6}
+set2 ={4,5,6,6,7,2}   
+print(set1.union(set2))   #union of set1 and set2
+print(set1.intersection(set2))   #intersection of set1 and set2
+
+
+
+
+# print(set)
+# print(len(set))       #print length of set
